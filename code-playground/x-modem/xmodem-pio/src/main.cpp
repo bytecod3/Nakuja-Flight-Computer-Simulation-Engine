91d3a285ddff456627b643007cb2ffc1029fd44f
bytecod3 <emwiti658@gmail.com>
2024-07-05 16:38:09 +0300

    adds EOF

--- code-playground/x-modem/xmodem-pio/src/main.cpp
+++ code-playground/x-modem/xmodem-pio/src/main.cpp
@@ -20,11 +20,16 @@ unsigned long last_NAK_time = 0;
 unsigned long current_NAK_time = 0;
 
 /* define XMODEM commands in HEX */
-#define SOH     0x01    /*!< start of header */
-#define EOT     0x04    /*!< end of transmission */
-#define ACK     0x06    /*!< positive acknowledgement */
-#define NAK     0x15    /*!< negative acknowledgement */
-#define CAN     0x18    /*!< cancel */
+#define SOH     "0x01"    /*!< start of header */
+#define EOT     "0x04"    /*!< end of transmission */
+#define ACK     "0x06"    /*!< positive acknowledgement */
+#define NAK     "0x15"    /*!< negative acknowledgement */
+#define CAN     "0x18"    /*!< cancel */
+
+#define MAX_CSV_LENGTH 10 /*!< Maximum length of the csv string that can be received */
+
+char serial_buffer[MAX_CSV_LENGTH];
+int16_t serial_index = 0;
 
 /**
  * XMODEM serial function prototypes
@@ -45,27 +50,43 @@ void ParseSerial(char* serialString);
  *******************************************************************************/
 void InitXMODEM() {
 
+    // check if the transmitter has acknowledged our call
     Serial.begin(BAUDRATE);
-    Serial.println("0x15");
+    Serial.print(NAK);
+    Serial.print("\n");
     Serial.flush();
     
 }
 
-/**
- * Receive and process a serial commmand
- */
-void SerialEvent() {
-    while (Serial.available()) {
 
+/*!****************************************************************************
+ * @brief Receive and process a serial command
+ *******************************************************************************/
+void SerialEvent() {
+    while (Serial.available() > 0) {
+        char ch = Serial.read();
+
+        if(serial_index < MAX_CSV_LENGTH) {
+            serial_buffer[serial_index++] = ch;
+        } else {
+            serial_buffer[serial_index] = 0;
+        }
+
+        if(ch == '\n') {
+            serial_index = 0;
+            Serial.println(serial_buffer);
+        }
+        
     }
 
 }
 
 void setup() {
-     
+     Serial.begin(BAUDRATE);
 }
 
 void loop() {
+    SerialEvent();
 
     current_NAK_time = millis();
     if( (current_NAK_time - last_NAK_time) > NAK_INTERVAL) {
--- code-playground/x-modem/xmodem-pio/src/main.py
+++ code-playground/x-modem/xmodem-pio/src/main.py
@@ -1,11 +1,34 @@
 import serial
+import random
 
+EOT = "0x04"
+ACK = "0x06"
+CAN = "0x18"
+NAK = "0x15"
+SOH = "0x01"
+
+data = "" # to hold the data packet
 
 # open serial connection
 ser = serial.Serial("COM10", 115200) #TODO: make COM port dynamic
 while 1:
-    rx_cmd = ser.readline()
+    rx_cmd = ser.readline().decode(encoding="utf-8").rstrip()
 
     print(rx_cmd)
+
+    # parse the received command
+    if rx_cmd == NAK :
+        # create the message packet
+        # <SOH><BLK#><255-BLK#><---128 DATA BYTES---><CHECKSUM>
+        ser.write(SOH.encode()) # start the header, for this implementation we do not need to have the BLK number etc. 
+        d = random.randint(0,67) 
+        data + str(d)
+        ser.write(data.encode())
+        ser.write(EOT.encode()) # end of header
+
+        # ser.close()
+
+
+        
 
 
