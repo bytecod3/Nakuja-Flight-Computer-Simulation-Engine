9e7fef0b9cfb0a9b35953764574171c65cd99661
bytecod3 <emwiti658@gmail.com>
2024-09-19 16:22:38 +0300

    adds terminal velocity calculation

--- matlab-data-generators/GenerateV.m
+++ matlab-data-generators/GenerateV.m
@@ -12,7 +12,7 @@
 % v_coast Velocity at coasting flight stage
 %
 
-function [v_powered, v_coast] = GenerateV(v_initial, g, t_flight)
+function [v_powered, v_coast, negative_velocity, terminal_velocity] = GenerateV(v_initial, g, t_flight)
     
     % generating time vectors 
     t = 0:1:t_flight;
@@ -24,10 +24,25 @@ function [v_powered, v_coast] = GenerateV(v_initial, g, t_flight)
 
     % velocity equation 
     % calculate velocity for powered flight. The acceleration due to gravity is +ve
-    v_powered = v_initial + g .* p_flight_vec; 
+    v_powered = v_initial + g * p_flight_vec; 
 
     % calculate velocity for coasting. The acceleration due to gravity is -ve
     % velocity for coasting starts at max velocity for powered 
-    v_coast = v_powered(end) + (-g) .* p_flight_vec;
+    v_coast = v_powered(end) + (-g) * p_flight_vec;
+    
+    % generate the acceleration downwards after apogee
+    % 12 to 18 in steps of 1
+    x = 12:1:18;
+
+    % ideal accelration in the -ve direction from apogee
+    y = x .* 2;
+    negative_velocity = -1 * y;
+    
+    % terminal velocity
+    % terminal velocity is a constant 
+    x = 18:1:24;
+    y_terminal = ones(length(x));
+    terminal_velocity = y(end) .* y_terminal;
+
     
 end 
--- matlab-data-generators/GenerateVelocityTest.m
+++ matlab-data-generators/GenerateVelocityTest.m
@@ -1,15 +1,56 @@
-v1 = 0; % initial velocity
-g = 9.8; % acceleration due to gravity 
-t_flight = 12; % time of flight 
+% v1 = 0;         % initial velocity
+% g = 9.8;        % acceleration due to gravity 
+% t_flight = 12;  % time of flight 
+% 
+% [v_p, v_c] = GenerateV(v1, g, t_flight);
+% 
+% p_flight_vec = 0:1:6;
+% c_flight_vec = 6:1:t_flight;
+% 
+% disp("V Powered");
+% disp(v_p);
+% disp("V Coasting");
+% disp(v_c);
 
+% plot the curve
+% figure(1);
+% plot(p_flight_vec, v_p); hold on;
+% plot(c_flight_vec, v_c)
+% %plot(c_flight_vec, v_c);
+% title("Rocket velocity data");
+% xlabel("Time (s)");
+% ylabel("Velocity (m/s)");
+% legend("Powered-flight", "Coasting");
 
-[v_p, v_c] = GenerateVelocity(v1, g, t_flight);
+Fs = 1000;
+Ts = 1/Fs;
+t = 0:Ts:3;
+t = t(:);
 
-% plot the curve 
-figure(1);
-plot(p_flight_vec, v_p); hold on;
-plot(c_flight_vec, v_c);
-title("Rocket velocity data");
-xlabel("Time (s)");
-ylabel("Velocity (m/s)");
-legend("Powered-flight", "Coasting");
+% 12 to 18 in steps of 1
+x = 12:1:18;
+
+% ideal accelration in the -ve direction from apogee
+y = x .* 2;
+y = -1 * y;
+
+plot(x, y);
+hold on;
+
+% terminal velocity
+x = 18:1:24; 
+y_terminal = ones(length(x));
+y = y(end) .* y_terminal;
+
+plot(x, y, 'r');
+title("Model rocket fall");
+legend("Negative deceleration", "Terminal velocity");
+hold on;
+
+% x = 0.5; % x can be any number greater than 1. Use 1 for linear 
+% a = linspace(18, 12, 6); a(:);
+% fadeout = a .^ x;
+% fadeout = -1 .* fadeout;
+
+% a = linspace(18, 12, 6); a(:);
+% fadein = a .^ x;
