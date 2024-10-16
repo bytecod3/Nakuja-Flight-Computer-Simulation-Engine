9df9dd3026d39a10707d95c10394f0a65d30be16
bytecod3 <emwiti658@gmail.com>
2024-10-16 13:08:13 +0300

    adds x cceleration plotter

--- matlab-data-generators/generate_altitude.m
+++ matlab-data-generators/generate_altitude.m
@@ -9,7 +9,7 @@ time = 0:0.001:50; % half the flight time is 50 seconds, 50 seconds to reach apo
 time = time(2:end); 
 
 max_alt = 4000; % 4km 
-min_alt = 0; 
+min_alt = 0;
 
 a = linspace(0, max_alt, samples/2); a = a(:);
 x = 1.5;
@@ -20,7 +20,6 @@ powered_flight = 0.05*a.^x; % exponential growth function
 t = 50:0.001:100;
 t = t(2:end);
 desc = linspace(powered_flight(end), 0, samples/2); desc = desc(:);
-
 
 plot(time, powered_flight, t, desc);
 xlabel( 'x' )
