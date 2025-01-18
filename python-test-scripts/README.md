cb32466c647528f20c0529e0a1b7df3293e0d2ce
bytecod3 <emwiti658@gmail.com>
2025-01-18 13:04:29 +0300

    Updates python READMe

---
+++ python-test-scripts/README.md
@@ -0,0 +1,56 @@
+## NAKUJA INTEGRATED FLIGHT COMPUTER TESTING SYSTEM 
+### TEST DATA VERIFICATION TOOL v1.0
+
+### Purpose 
+This is a python console tool used as a bridge between simulation data from MATLAB and 
+the C++ simulation GUI interface. 
+The purpose of this tool is to verify the test data by generating graphs and generating 
+flight state machine log file. 
+The tool allows the user to detect various states of flight and verify that 
+indeed the data from MATLAB generates the needed states.  
+Following this, this tool can generate the flight state log that can then be analyzed
+by the tester. 
+
+The main test data used here is the altitude data, which we heavily rely on to 
+detect the flight states.
+
+### Requirements 
+Make sure you have installed the following in your PC
+``` python
+1. Python >3.10
+2. Matplotlib
+3. Numpy
+```
+
+### Test data preparation 
+This tool requires test data to be used as intended. To prepare the data, follow the steps 
+below: 
+1. Copy the ```altitude_log.csv``` and ```altitude_data_matlab.csv``` files from the ```matlab-data-generators``` folder and 
+paste into ```python-test-scripts``` folder. These two files are essential for 
+carrying out the test. 
+
+|File | Description                                                                                                                                                  |
+|---|--------------------------------------------------------------------------------------------------------------------------------------------------------------| 
+|```altitude_log.csv```| Contains a single column that represents altitude data                                                                                                       |
+|```altitude_data_matlab.csv```| Might contain multiple columns, with the ***First column*** representing the row index, the second column ***MUST*** represent the altitude values in meters |
+
+NOTE: Please make sure the files are named exactly as described above
+
+### Usage 
+Follow the steps below to run the tool:
+1. Open a console inside the ```python-test-scripts``` folder
+2. Run the following command in the console  
+``` python main.py```. The following console will appear.
+
+3. Select the required option and follow the displayed prompts. 
+
+### Data analysis
+The data produced by the tool is two fold:
+- Graphical data
+- State log file 
+
+You can save the graphs as images for later usage.  
+For the flight state file, open the ```flight_test_file.txt``` and verify that the 
+flight states are in sequence as intended.
+
+
--- python-test-scripts/main.py
+++ python-test-scripts/main.py
@@ -19,17 +19,6 @@ altitude_val = [] # holds altitude values
 altitude_x1 = [] # holds x index values for a file with multiple columns
 altitude_x = []  # holds x values for file with a single column
 
-def open_csv(filename, has_multiple_columns = 1):
-    with open(filename, newline='') as csvfile:
-        filereader = csv.reader(csvfile, delimiter=',')
-
-        if has_multiple_columns:
-            for row in filereader:
-                altitude_x1.append(row[0]) # represents x index
-                altitude_val.append(row[1]) # second row represents the altitude data
-        else:
-            for row in filereader:
-                altitude_val.append(row[0]) # this is just for confirmation using different altitude data
 
 def generate_altitude_plot(has_header = 1):
     alt_float = [] # to hold converted values from altitude list
@@ -49,6 +38,9 @@ def generate_altitude_plot(has_header = 1):
 
         # plot
         plt.plot(altitude_x, alt_float)
+        plt.xlabel("x-index")
+        plt.ylabel("altitude (meters)")
+        plt.title("Simulated altitude graph")
         plt.show()
 
     else: # for file with multiple columns
@@ -62,11 +54,39 @@ def generate_altitude_plot(has_header = 1):
         for j in range(0, len(lx)):
             x_float.append(float(lx[j]))
 
-
         # plot
         plt.plot(x_float, alt_float)
+        plt.xlabel("x-index")
+        plt.ylabel("altitude (meters)")
+        plt.title("Simulated altitude graph")
         plt.show()
 
+
+def open_csv(filename):
+    has_multiple_columns = 0
+    # check filename for multiple columns
+    file_with_multi_columns = "altitude_data_matlab.csv"
+    file_single_columns = "altitude_log.csv"
+
+    if filename == "altitude_data_matlab.csv":
+        has_multiple_columns = 1
+    elif filename == "altitude_log.csv":
+        has_multiple_columns = 0
+
+    with open(filename, newline='') as csvfile:
+        filereader = csv.reader(csvfile, delimiter=',')
+
+        if has_multiple_columns:
+            for row in filereader:
+                altitude_x1.append(row[0]) # represents x index
+                altitude_val.append(row[1]) # second row represents the altitude data
+
+            generate_altitude_plot(has_header=0)
+        else: # has single column, no header
+            for row in filereader:
+                altitude_val.append(row[0]) # this is just for confirmation using different altitude data
+
+            generate_altitude_plot(has_header=1)
 
 
 '''
@@ -74,6 +94,51 @@ normal operation
 use file with 2 columns, second column representing the altitude values
 '''
 
-open_csv("altitude_data_matlab.csv")
-print(altitude_x1)
-generate_altitude_plot(has_header=0)
+#============  use this for file with a single column ============
+# open_csv("altitude_log.csv")
+# generate_altitude_plot(has_header=1)
+
+#============ use this for file with multiple columns ============
+# open_csv("altitude_data_matlab.csv")
+# generate_altitude_plot(has_header=0)
+
+# menu options
+menu_options = {
+    1: "Generate altitude plot data",
+    2: "Generate flight state log from simulated data",
+    3: "Exit"
+}
+
+def run_console():
+
+    i = 100
+    header = "NAKUJA INTEGRATED TESTING SYSTEM"
+    subheader = "TEST DATA VERIFICATION TOOL v1.0"
+    a = len(header)
+    b = i-a
+
+    print("="*int(b/2)+ " " +header+ " "+ "="*int(b/2))
+    print(" "*int(b/2)+ " " +subheader+ " "+ " "*int(b/2))
+    print("=" * i)
+    print("Choose an item from the menu below:")
+
+    # show menu
+    for n, option in menu_options.items():
+        print(str(n) + "." + option)
+
+    choice = eval(input(">"))
+
+    if choice == 1:
+        # TODO: check if filename is valid, if it is a csv file
+        f = input("Enter filename: ")
+        open_csv(f)
+
+    elif choice == 2:
+        print("Choice 2")
+    elif choice == 3:
+        print("Choice 3")
+
+
+
+if __name__ == "__main__":
+    run_console()
